ca7bbf97f0da966883bb9326735c0eda032c9abb
Pelumiade <fecoyifemi@gmail.com>
2023-08-24 17:18:12 +0100

    add

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -137,9 +137,9 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-                    ]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static/',
+#                     ]
 
 # STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
