4faf0331d9fb20c4665c4d5ed88d2ab83a263096
Pelumiade <fecoyifemi@gmail.com>
2023-08-28 18:01:01 +0100

    update

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -20,7 +20,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-DATABASE_URL = config('DATABSE_URL')
+DATABASE_URL = config('DATABASE_URL')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
