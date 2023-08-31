dad964ab105112c84edca9b574b582a601220c36
Pelumiade <fecoyifemi@gmail.com>
2023-08-31 17:44:44 +0100

    update

--- ecommerce_app/serializers.py
+++ ecommerce_app/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from .models import Product, CartItem, Order, ShippingMethod, DeliveryDetails, Payment, Country
-
+from typing import Dict, Any
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
 
 class ProductCartSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,34 +14,26 @@ class ProductCartSerializer(serializers.ModelSerializer):
         fields = ('name', 'image', 'new_price')
 
 
-# class CartItemSerializer(serializers.ModelSerializer):
-#     product = ProductCartSerializer()
-#     item_price = serializers.SerializerMethodField() 
-
-#     class Meta:
-#         model = CartItem
-        
-#         fields = ('product', 'quantity', 'item_price')  
-
-
-#     def get_item_price(self, obj):
-#         return obj.product.new_price * obj.quantity  
-    
-#     def get_item_price(self, obj: CartItem) -> float:
-#         return obj.product.new_price * obj.quantity
-
 class CartItemSerializer(serializers.ModelSerializer):
-    product_id = serializers.IntegerField(write_only=True) 
+    product = serializers.SerializerMethodField() 
+    product_id = serializers.IntegerField(write_only=True)
     item_price = serializers.SerializerMethodField()
+    quantity = serializers.IntegerField(default=1) 
 
     class Meta:
         model = CartItem
-        fields = ('product_id', 'quantity', 'item_price')
+        fields = ('product', 'product_id', 'quantity', 'item_price')
 
-    def get_item_price(self, obj):
+    def get_product(self, obj: CartItem) -> Dict[str, Any]:
+        return {
+            'name': obj.product.name,
+            'image': obj.product.image.url, 
+            'new_price': obj.product.new_price,
+        }
+
+    def get_item_price(self, obj: CartItem) -> float:
         return obj.product.new_price * obj.quantity
-
-
+    
 
 class CountrySerializer(serializers.ModelSerializer):
     class Meta:
